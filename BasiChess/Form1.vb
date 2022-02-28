eda05bacd3bb865674a58297e10e611db13165bd
Jacob MacLeod <jacob.e.macleod@gmail.com>
2022-02-28 11:43:15 +0000

    Added indicator to show where pawns can move

--- BasiChess/Form1.vb
+++ BasiChess/Form1.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form1
-    Dim gameGrid As String()() = {
+    Public gameGrid As String()() = {
     ({"♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜"}),
     ({"♟", "♟", "♟", "♟", "♟", "♟", "♟", "♟"}),
     ({" ", " ", " ", " ", " ", " ", " ", " ", " "}),
@@ -11,9 +11,57 @@
 
     Dim btncollection As New Microsoft.VisualBasic.Collection()
 
-    Public Sub ChangeText(button As Button)
+    '  Coordinates must be in x,y format
+    Public Function CheckIfMoveIsValid(Piece As String, Coordinates As Integer())
+        Dim CoordinatesToReturn As Integer()()
 
-    End Sub
+        ' Pawns can move one space ahead diagonly
+        If Piece Is "♙" Then
+            ' X, y Format - Return coordinates where the preice can be moved to
+            CoordinatesToReturn = {({Coordinates(0) - 1, Coordinates(1) + 1}), ({Coordinates(0) + 1, Coordinates(1) + 1})}
+            Return CoordinatesToReturn
+        Else
+            CoordinatesToReturn = {({0, 0}), ({0, 0})}
+            Return CoordinatesToReturn
+        End If
+    End Function
+
+    ' Converts a button name such as Button24 to a set of coordinates which it returns
+    Public Function ConvertButtonToCoordinates(ButtonName As String)
+        Dim Str_ButtonNumber As String
+        Dim Int_ButtonNumber As Integer
+        Dim NearestMultiple As Integer
+        Dim XCoord As Integer
+        Dim YCoord As Integer
+
+        ' Get the number of the button in ButtonName which wil be in the format ("Button", Number) conjoined as a String
+        Str_ButtonNumber = ButtonName.Split("n"c)(1)
+        Int_ButtonNumber = CStr(Str_ButtonNumber)
+
+        ' Find the nearest multiple of 8 to Int_ButtonNumber which is higher than Int_ButtonNumber
+        For i As Integer = 0 To 7
+            If ((Int_ButtonNumber + i) Mod 8) = 0 Then
+                ' This line is garunteed to be reached eventually
+                NearestMultiple = Int_ButtonNumber + 1
+                ' This will be a whole number
+                NearestMultiple = NearestMultiple / 8
+            End If
+        Next
+
+        ' Find the x Coordinate
+        XCoord = NearestMultiple * 8
+        XCoord = XCoord - Int_ButtonNumber
+        XCoord = 8 - XCoord
+
+        ' Find the y Coordinate
+        YCoord = Int_ButtonNumber - XCoord
+        YCoord = YCoord / 8
+        YCoord = YCoord + 1
+
+        ' Return the coordinates as an array
+        Dim CoordsToReturn = New Integer(1) {XCoord, YCoord}
+        Return CoordsToReturn
+    End Function
 
     ' Apply the values in gamegrid to all the buttons, changing their icon
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -40,9 +88,31 @@
     Dim buttonClicked As Button
 
     ' Moving of pieces in chess
-    ' Runs whenever a button is clicked. When two buttons are clicked it swaps them around
-    ' TODO: Make it so they only swap around if the rules of chess allow them to
+    ' Runs whenever a button is clicked. When two buttons are clicked one takes the other
+    ' TODO: Make it so they only do this if the rules of chess allow them to
     Public Sub ButtonOnClickEvent(sender)
+        Dim Coords As Integer()
+        Dim CoordsReturned As Integer()()
+        ' Dummy data so far - I next need to make it 
+        Coords = {7, 5}
+
+        ' Returns coordinates showing where a piece can be moved
+        CoordsReturned = CheckIfMoveIsValid(sender.Text, ConvertButtonToCoordinates(sender.Name))
+        For x As Integer = 0 To 1
+            ' I now know where a specific piece can move
+            Dim ButtonWherePieceCanBeMoved As String
+            ButtonWherePieceCanBeMoved = "Button"
+            ' I need to turn the coords given by CheckIfMoveIsValid() (say 6, 4) into a number corrosponding to the button name from 1 to 64 so i can select Button + number
+            ' as in Button1. This is done mathematically, as in
+            ButtonWherePieceCanBeMoved = ButtonWherePieceCanBeMoved + (((((CoordsReturned(x)(1) - 1) * 8) + CoordsReturned(x)(0)).ToString())).ToString()
+            For Each ctl In Me.Controls
+                ' When know where the piece can be moved to
+                If CType(ctl.Name, String).ToString().Contains(ButtonWherePieceCanBeMoved) Then
+                    ctl.BackColor = Color.Red
+                End If
+            Next
+        Next
+
         If WasButtonClicked = True Then
             ' To take another piece
             If buttonClicked.Text IsNot Nothing Then
@@ -56,7 +126,7 @@
                 WasButtonClicked = False
             End If
         Else
-                valueOfButtonClicked = sender.Text
+            valueOfButtonClicked = sender.Text
             buttonClicked = sender
             WasButtonClicked = True
         End If
